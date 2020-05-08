020dc03c2fec2b7d25d44a815922639b462f3604
fedorkolmykow <fedorkolmykow@gmail.ru>
2020-05-09 02:38:29 +0300

    Ручки

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from app import db
+from flask_serialize import FlaskSerializeMixin
 
 
-class Client(db.Model):
+
+class Client(FlaskSerializeMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone = db.Column(db.String(12))
     passport = db.Column(db.String(11))
@@ -12,7 +14,7 @@ class Client(db.Model):
         return f'<Client {self.name}>'
 
 
-class Delivery(db.Model):
+class Delivery(FlaskSerializeMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
     items = db.relationship('Item', backref='delivery', lazy='dynamic')
@@ -22,7 +24,7 @@ class Delivery(db.Model):
         return f'<Delivery {self.id}>'
 
 
-class Item(db.Model):
+class Item(FlaskSerializeMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     delivery_id = db.Column(db.Integer, db.ForeignKey('delivery.id'))
     barcode = db.Column(db.String(12))
@@ -32,7 +34,7 @@ class Item(db.Model):
         return f'<Item {self.id}>'
 
 
-class Return(db.Model):
+class Return(FlaskSerializeMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
 
@@ -49,7 +51,7 @@ class CellDelivery(db.Model):
         return f'<Cell Delivery {self.id}>'
 
 
-class Cell(db.Model):
+class Cell(FlaskSerializeMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cell_deliveries = db.relationship('CellDelivery', backref='cell', lazy='dynamic')
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,54 @@
 from app import app
+from app import models
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+
+@app.route('/client/<int:item_id>', methods=['GET', 'DELETE'])
+@app.route('/clients', methods=['GET'])
+def items(item_id=None):
+    return models.Client.get_delete_put_post(item_id)
+
+
+@app.route('/clients', methods=['POST'])
+def post_clients(item_id=None):
+    return models.Client.get_delete_put_post(item_id)
+
+
+@app.route('/clients', methods=['PUT'])
+def put_clients(item_id=None):
+    return models.Client.get_delete_put_post(item_id)
+
+
+@app.route('/delivery/<int:item_id>', methods=['GET', 'DELETE'])
+@app.route('/deliveries', methods=['GET'])
+def get_delivery(item_id=None):
+    return models.Delivery.get_delete_put_post(item_id)
+
+
+@app.route('/item/<int:item_id>', methods=['GET', 'DELETE'])
+@app.route('/items', methods=['GET'])
+def get_item(item_id=None):
+    return models.Item.get_delete_put_post(item_id)
+
+
+@app.route('/return/<int:item_id>', methods=['GET', 'DELETE'])
+@app.route('/returns', methods=['GET'])
+def get_return(item_id=None):
+    return models.Return.get_delete_put_post(item_id)
+
+
+@app.route('/cell_delivery/<int:item_id>', methods=['GET', 'DELETE'])
+@app.route('/cells_deliveries', methods=['GET'])
+def get_celldelivery(item_id=None):
+    return models.CellDelivery.get_delete_put_post(item_id)
+
+
+@app.route('/cell/<int:item_id>', methods=['GET', 'DELETE'])
+@app.route('/cells', methods=['GET'])
+def get_cell(item_id=None):
+    return models.Cell.get_delete_put_post(item_id)
