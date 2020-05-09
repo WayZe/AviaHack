3dc89fbc717d367cd979339a481b101d128471a7
Андрей Макаров <anmakarov@ivi.ru>
2020-05-09 15:23:40 +0300

    Добавил ручку выдачи товара

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from flask import request
-from app import app
-from app import models
+from typing import List
+
+from flask import jsonify, request
+
+from app import app, models
+from app.models import Delivery
 
 
 @app.route('/')
@@ -48,8 +51,38 @@ def get_cell(item_id=None):
 @app.route('/put_item', methods=['POST'])
 def put_item():
     _barcode = request.form.get('barcode')
-    item = models.Item.query.filter_by(barcode=f'{_barcode}').first()
-    delivery_cells = models.Item.query.filter_by(delivery=f'{item.delivery}')
+    item = models.Item.query.filter_by(barcode=_barcode).first()
+    delivery_cells = models.Item.query.filter_by(delivery=item.delivery)
     for c in delivery_cells:
         pass
     return str(item.id)
+
+
+@app.route('/give_item', methods=['POST'])
+def give_item():
+    """Получение списка товаров по телефону и ФИО
+
+    Пример запроса: curl -X POST -d "phone=12"  localhost:5000/give_item
+    """
+    phone = request.form.get('phone')  # type: str
+    name = request.form.get('fio')  # type: str
+    jsons = {}  # type: Dict
+
+    if phone is not None:
+        client_id = models.Client.query.filter_by(phone=phone).first().id  # type: int
+    elif name is not None:
+        client_id = models.Client.query.filter_by(name=name).first().id  # type: int
+    else:
+        print('Переданы неизвестные параметры')
+        return {}
+
+    deliveries = models.Delivery.query.filter_by(client_id=client_id).all()  # type: List[Delivery]
+    for delivery in deliveries:
+        items1 = models.Item.query.filter_by(delivery_id=delivery.id).all()
+        dev = {}
+        for item in items1:
+            dev[f'Item {item.id}'] = item.__dict__
+            del dev[f'Item {item.id}']['_sa_instance_state']
+        jsons[f'Delivery {delivery.id}'] = dev
+
+    return jsonify(str(jsons).replace('\'', ''))
