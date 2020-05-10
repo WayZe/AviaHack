ffd6206a55ec4406a3cbf8b2cff495d3853103a0
Андрей Макаров <anmakarov@ivi.ru>
2020-05-10 12:05:03 +0300

    Добавил postgres в зависимости

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'postgres://postgres:1234@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
